72f8359ec249033a5529a53db734bac9be5ad81b
Ayoobe <akremiayoub@insat.u-carthage.tn>
2024-04-01 12:15:15 +0100

    finished registration

--- register.php
+++ register.php
@@ -1,3 +1,52 @@
+<?php
+include ('server/connection.php');
+session_start();
+if(isset($_POST['register'])){
+  
+    $Name=$_POST['Name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $confirmPassword=$_POST['confirmPassword'];
+
+  if($password!=$confirmPassword){
+        header('location: register.php?error=Passwords do not match');
+    }
+  else if(strlen($password)<6){
+        header('location: register.php?error=Password must be at least 6 characters long');}
+
+  else{
+      //check if email already exists
+    $stmt1=$conn->prepare("SELECT * FROM users WHERE user_email=?");
+    $stmt1->bind_param("s", $email);
+    $stmt1->execute();
+    $result=$stmt1->get_result();
+    $row=$result->fetch_assoc();
+    if($row){
+        header('location: register.php?error=Email already exists');
+    }
+
+    else{
+        $stmt=$conn->prepare("INSERT INTO users (user_name, user_email, user_password) VALUES (?,?,?)");
+        $stmt->bind_param("sss", $Name, $email, md5($password));
+        $stmt->execute();
+        $_SESSION['user_email']=$email;
+        $_SESSION['user_name']=$Name;
+        $_SESSION['logged_in']=true;
+        header('location: account.php?registration=Successfully registered');
+    }
+  }
+}
+
+else if(isset($_SESSION['logged_in'])){
+    header('location: account.php');
+    exit();
+}
+    
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -53,7 +102,8 @@
     <hr class="mx-auto">
   </div>
   <div class="mx-auto container">
-    <form id="register-form">
+    <form id="register-form" method="POST" action="register.php">
+        <p style="color: red;"><?php if(isset($_GET['error'])){echo $_GET['error'];}?></p>
         <div class="form-group">
             <label >Name</label>
             <input type="text" class="form-control" id="register-Name" name="Name" placeholder="Name" required>
@@ -74,10 +124,10 @@
 
 
       <div class="form-group">
-        <input type="Submit" class="btn" id="login-btn" value="Register">
+        <input type="Submit" class="btn" id="login-btn" name="register" value="Register">
       </div>
       <div class="form-group">
-        <a id="register-url" class="btn" href="">Already a member ? Log in Now!</a>
+        <a id="register-url" class="btn" href="login.php">Already a member ? Log in Now!</a>
 
       </div>
 

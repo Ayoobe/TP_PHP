e306d113d5eebe219e740635cb247a76fc6e5dd7
Ayoobe <akremiayoub@insat.u-carthage.tn>
2024-04-01 16:02:04 +0100

    update account

--- login.php
+++ login.php
@@ -1,3 +1,38 @@
+<?php 
+session_start();
+include ('server/connection.php');
+if(isset($_POST['login_btn'])){
+  $email=$_POST['email'];
+  $password=$_POST['password'];
+
+  $stmt=$conn->prepare("SELECT * FROM users WHERE user_email=? AND user_password=?");
+  $stmt->bind_param("ss", $email, md5($password));
+  $stmt->execute();
+  $result=$stmt->get_result();
+  $row=$result->fetch_assoc();
+  if($row){
+    $_SESSION['user_email']=$email;
+    $_SESSION['user_name']=$row['user_name'];
+    $_SESSION['user_id']=$row['user_id'];
+    $_SESSION['logged_in']=true;
+    header('location: index.php');
+  }
+  else{
+    header('location: login.php?error=Incorrect email or password');
+  }
+
+
+}
+    
+
+
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -53,7 +88,8 @@
     <hr class="mx-auto">
   </div>
   <div class="mx-auto container">
-    <form id="login-form">
+    <form id="login-form" action="login.php" method="POST">
+      <p style="color: red" class="text-center"><?php if(isset($_GET['error'])){echo $_GET['error'];} ?></p>
       <div class="form-group">
         <label >Email</label>
         <input type="text" class="form-control" id="login-email" name="email" placeholder="Email" required>
@@ -63,7 +99,7 @@
         <input type="password" class="form-control" id="login-password" name="password" placeholder="Password" required>
       </div>
       <div class="form-group">
-        <input type="Submit" class="btn" id="login-btn" value="login">
+        <input type="Submit" class="btn" id="login-btn" name="login_btn" value="login">
       </div>
       <div class="form-group">
         <a id="register-url" class="btn" href="">Not a member ? Sign Up Now!</a>

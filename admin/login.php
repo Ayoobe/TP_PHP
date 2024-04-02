73785cf6315a6d4836893c625a8324bfb5b65c02
achrefbenammar546372819 <achref.benammar404@gmail.com>
2024-04-02 19:07:22 +0100

    login logout admin

--- admin/login.php
+++ admin/login.php
@@ -39,54 +39,7 @@
 
 
 
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-  </head>
-
-
-  <body>
-    <!-- start of navbar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 fixed-top">
-      <div class="container">
-        <img src="assets/imgs/logo.jpg" >
-        <a class="navbar-brand" href="index.html">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse nav-buttons" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="reserve.html">Reserve</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact</a>
-            </li>
-    
-            <li class="nav-item">
-              <a class="nav-link" href="account.html">
-                <i class="fas fa-user"></i>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="cart.html">
-                <i class="fas fa-shopping-cart"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
+  <?php include('header.php'); ?>
   <!--login-->
   <section class="my-5 py-5">
     <div class="container text-center mt-3 pt-5">

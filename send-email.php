34567a6c8952332cda38c5cebd6eb7ab035f6a38
Achraf Ben Ammar <107804419+achrefbenammar404@users.noreply.github.com>
2024-04-10 22:15:42 +0100

    Update send-email.php

--- send-email.php
+++ send-email.php
@@ -1,46 +1,47 @@
 <?php
-session_start(); // Start the session
 
-// Check if the CSRF token is set in the session
-if (!isset($_SESSION['csrf_token'])) {
-    die("CSRF token is not set in the session.");
-}
+require 'vendor/autoload.php';
 
-// Check if the CSRF token is present in the form submission
-if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
-    die("CSRF token validation failed!");
-}
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Check if form is submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Sanitize form data
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $subject = htmlspecialchars($_POST['subject']);
-    $message = htmlspecialchars($_POST['message']);
+$name = $_POST['name'] ?? '';  // Assign empty string if not set
+$email = $_POST['email'] ?? '';
+$subject = $_POST['subject'] ?? '';
+$message = $_POST['message'] ?? '';
 
-    // Retrieve admin email from environment variable
-    $admin_email = $_ENV['ADMIN_EMAIL'];
+$emailSent = false;
 
-    // Email headers
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-    // Send email
-    if (mail($admin_email, $subject, $message, $headers)) {
-        // Redirect to contact page with success message
-        header("Location: contact.php?success=true");
-        exit;
-    } else {
-        // Redirect to contact page with error message
-        header("Location: contact.php?success=false");
-        exit;
-    }
+if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+  $error = "Please fill out all required fields.";
 } else {
-    // Redirect to contact page if form is not submitted
-    header("Location: contact.php");
-    exit;
+  try {
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = "smtp.gmail.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "adefthukom0@gmail.com";  
+    $mail->Password = "trvq dvhk vsee hplt";  
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+
+    $mail->setFrom($mail->Username, "Tiskerti Contact Page"); 
+
+    $mail->addAddress('achref.benammar404@gmail.com');
+
+    $mail->isHTML(false);
+    $mail->Subject = $subject;
+    $mail->Body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
+
+    $mail->send();
+    
+    $emailSent = true;
+  } catch (Exception $e) {
+    $error = "Error sending email: " . $mail->ErrorInfo;
+  }
 }
-?>
+
+// Return JSON response
+header('Content-Type: application/json');
+echo json_encode(['emailSent' => $emailSent, 'error' => isset($error) ? $error : null]);
